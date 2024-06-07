f69e552f8e53fcb398b1d86a3791204db3efa16c
Ambreen Riaz <159724062+Ambreenriaz13@users.noreply.github.com>
2024-06-07 23:36:27 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,16 @@
+let userNames = ["Asif", "Ziyam", "Afnan","Admin", "Adnan"]
+
+userNames =[ ]
+if (userNames .length === 0){
+    console.log("Your Array is Empty we need to find some users!")
+}else{
+// Using ForEach  Loop on Array
+userNames.forEach(oneUser =>{
+    if(oneUser === "Admin"){
+        console.log(`Hello ${oneUser}, would you like to see a status report?`)
+    }else{
+        console.log(`Hello ${oneUser}, thankyou for logging in again`)
+    }
+}
+)
+    }
